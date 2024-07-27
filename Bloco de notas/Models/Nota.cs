6ba1c0cc955a71319ee2785d9b4742e47d30a548
jp1204GORDOX <jpgertto.fernadesz1204@gmail.com>
2024-07-27 20:31:16 -0300

    Adicionado o model de notas

---
+++ Bloco de notas/Models/Nota.cs
@@ -0,0 +1,7 @@
+﻿namespace Bloco_de_notas.Models {
+    public class Nota {
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataAlteracao { get; set; }
+        public string Texto{ get; set; }
+    }
+}
--- Bloco de notas/obj/Debug/net8.0/Bloco de notas.AssemblyInfo.cs
+++ Bloco de notas/obj/Debug/net8.0/Bloco de notas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bloco de notas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c306259df3ecfa55719d3b56e150e33c195cb285")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bloco de notas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bloco de notas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
