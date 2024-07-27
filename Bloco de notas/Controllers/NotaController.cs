eaf33275cc2386dd503e51d01b0e76ca446f3810
jp1204GORDOX <jpgertto.fernadesz1204@gmail.com>
2024-07-27 20:41:55 -0300

    Criado o controller de nota

---
+++ Bloco de notas/Controllers/NotaController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bloco_de_notas.Controllers {
+    public class NotaController : Controller {
+        public IActionResult NovaNota() {
+            return View();
+        }
+    }
+}
