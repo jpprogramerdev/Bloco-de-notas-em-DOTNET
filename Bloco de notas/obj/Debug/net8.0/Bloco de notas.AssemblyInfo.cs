3f137afafc8729104902eb8eaeca87a2ae3e5820
jp1204GORDOX <jpgertto.fernadesz1204@gmail.com>
2024-07-27 21:03:52 -0300

    Adicionado a painga de nova ntoa

--- Bloco de notas/obj/Debug/net8.0/Bloco de notas.AssemblyInfo.cs
+++ Bloco de notas/obj/Debug/net8.0/Bloco de notas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bloco de notas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4640b790ff3b1adfc34e3a2663be1e482e9ddaed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaf33275cc2386dd503e51d01b0e76ca446f3810")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bloco de notas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bloco de notas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
