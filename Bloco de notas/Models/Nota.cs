28f90e768b7df0d9ce7f4f5c54a7bf04039e16eb
jp1204GORDOX <jpgertto.fernadesz1204@gmail.com>
2024-07-28 19:13:44 -0300

    Herença de EntidadeDominio

--- Bloco de notas/Models/Nota.cs
+++ Bloco de notas/Models/Nota.cs
@@ -1,5 +1,5 @@
 ﻿namespace Bloco_de_notas.Models {
-    public class Nota {
+    public class Nota : EntidadeDominio {
         public DateTime DataCriacao { get; set; }
         public DateTime DataAlteracao { get; set; }
         public string Texto{ get; set; }
