59a88dcf870862ce988d08db76946c6da6ce3576
jp1204GORDOX <jpgertto.fernadesz1204@gmail.com>
2024-07-28 10:33:13 -0300

    Adicionado para manipulação de dados

---
+++ Bloco de notas/DAO/Interfaces/IDAOGeneric.cs
@@ -0,0 +1,10 @@
+﻿using Bloco_de_notas.Models;
+
+namespace Bloco_de_notas.DAO.Interfaces {
+    public interface IDAOGeneric {
+        public List<EntidadeDominio> SelectAll();
+        public void Insert(EntidadeDominio entidade);
+        public void Update(EntidadeDominio entidade);
+        public void Delete(EntidadeDominio entidade);
+    }
+}
