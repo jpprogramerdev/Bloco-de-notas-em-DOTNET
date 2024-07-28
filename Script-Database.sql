ce4a1378e612d2f2494e416db9e80ef43a4305c4
jp1204GORDOX <jpgertto.fernadesz1204@gmail.com>
2024-07-28 19:48:23 -0300

    Alterado coluna da PK para ter auto incremento

--- Script-Database.sql
+++ Script-Database.sql
@@ -2,12 +2,14 @@ CREATE DATABASE BlocoDeNotas;
 
 USE BlocoDeNotas;
 
+DROP TABLE NOTAS;
+
 --Trigrama = NTS
 CREATE TABLE Notas(
-	NTS_Id int,
+	NTS_Id int IDENTITY (1,1),
 	NTS_Titulo varchar(500) not null,
 	NTS_Texto varchar(8000) not null,
 	NTS_DataCriacao Date not null,
 	NTS_DataUltimaAtualizacao Date not null,
-	PRIMARY KEY(NTS_Id)
+	CONSTRAINT PK_NTS PRIMARY KEY(NTS_Id)
 );
