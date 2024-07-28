d7545301f27d1bf3649992368e4848f3a616ffeb
jp1204GORDOX <jpgertto.fernadesz1204@gmail.com>
2024-07-28 10:09:02 -0300

    Criado a tabela de notas

--- Script-Database.sql
+++ Script-Database.sql
@@ -1,1 +1,13 @@
 CREATE DATABASE BlocoDeNotas;
+
+USE BlocoDeNotas;
+
+--Trigrama = NTS
+CREATE TABLE Notas(
+	NTS_Id int,
+	NTS_Titulo varchar(500) not null,
+	NTS_Texto varchar(8000) not null,
+	NTS_DataCriacao Date not null,
+	NTS_DataUltimaAtualizacao Date not null,
+	PRIMARY KEY(NTS_Id)
+);
