6a64faba668d69c77831f582a6d96d4aedc91d38
jp1204GORDOX <jpgertto.fernadesz1204@gmail.com>
2024-08-05 21:25:39 -0300

    Mudado o nome o titulo

--- Bloco de notas/obj/Debug/net8.0/Bloco de notas.AssemblyInfo.cs
+++ Bloco de notas/obj/Debug/net8.0/Bloco de notas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bloco de notas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6ed2395912456120ac09031c087c9245b7519fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65a5ebc5c01160810b1afb7ef4969cf7a444a313")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bloco de notas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bloco de notas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
