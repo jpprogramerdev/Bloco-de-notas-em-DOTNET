ed96ebebf3cec26dd223da8e60b631fc60242d07
jp1204GORDOX <jpgertto.fernadesz1204@gmail.com>
2024-08-10 08:47:47 -0300

    Mudado o id dos inputs

--- Bloco de notas/obj/Debug/net8.0/Bloco de notas.AssemblyInfo.cs
+++ Bloco de notas/obj/Debug/net8.0/Bloco de notas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bloco de notas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73c148515c23b54a60922c709b96ba06ab0808f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff08691ad748b9a671bd8af7aabe6d67bb50a697")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bloco de notas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bloco de notas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
